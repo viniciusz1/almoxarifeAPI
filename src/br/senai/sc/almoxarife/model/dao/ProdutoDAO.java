807f40ff36852ab587694609320ddc771521b27a
VINICIUS BONATTI BENNER <vinicius_benner@estudante.sesisenai.org.br>
2022-09-22 09:08:38 -0300

    Produtos

--- src/br/senai/sc/almoxarife/model/dao/ProdutoDAO.java
+++ src/br/senai/sc/almoxarife/model/dao/ProdutoDAO.java
@@ -1,11 +1,44 @@
 package br.senai.sc.almoxarife.model.dao;
 
 import br.senai.sc.almoxarife.model.entities.Produto;
+import br.senai.sc.almoxarife.model.factory.ConexaoFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class ProdutoDAO {
-    public Produto buscarTodosProdutos(){
+    public ArrayList<Produto> buscarTodosProdutos(){
+        ArrayList<Produto> listaDeProdutos = new ArrayList<>();
+        ConexaoFactory conexao = new ConexaoFactory();
+        Connection connection = conexao.conectaBD();
         String query = "select * from produtos";
+        PreparedStatement statement = connection.prepareStatement(query);
+        ResultSet resultset = statement.executeQuery();
+
+        while (resultset != null && resultset.next()) {
+            listaDeProdutos.add(extrairObjetoProduto(resultset));
+        }
+
         return null;
+    }
+
+    public Produto extrairObjetoProduto(ResultSet resultset){
+        try{
+            return new Produto(resultset.getInt("codigo"),
+                    resultset.getInt("quantidadeTotal"),
+                    resultset.getInt("quantidadeReservada"),
+                    resultset.getString("nome"),
+                    resultset.getString("classificacao"),
+                    resultset.getString("localidade"),
+                    resultset.getString("opcaoUso"),
+                    resultset.getString("descricao"),
+                    resultset.getString("imagem"));
+        }catch (Exception e) {
+            throw new RuntimeException("Erro ao extrair o objeto!");
+        }
+
     }
 
     public Produto buscarProdutoPorCodigo(){
--- src/br/senai/sc/almoxarife/model/factory/ConexaoFactory.java
+++ src/br/senai/sc/almoxarife/model/factory/ConexaoFactory.java
@@ -1,4 +1,4 @@
-package br.senai.sc.almoxarife.model.dao;
+package br.senai.sc.almoxarife.model.factory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
