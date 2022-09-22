94bafa77f683f539f4fd8d06c2c4be1de83b1aec
viniciusz1 <vinicius101313@gmail.com>
2022-09-21 21:09:02 -0300

    a

--- src/br/senai/sc/almoxarife/model/dao/PedidoDAO.java
+++ src/br/senai/sc/almoxarife/model/dao/PedidoDAO.java
@@ -1,5 +1,7 @@
 package br.senai.sc.almoxarife.model.dao;
 
+import br.senai.sc.almoxarife.model.entities.Produto;
+
 public class PedidoDAO {
 
     public Pedido buscarTodosPedidos(){
@@ -18,11 +20,20 @@ public class PedidoDAO {
         String query = "select * from pedidos where dataEntrada = ?";
     }
 
-    public Pedido BuscarDevolucaoPorData(){
+    public Pedido buscarDevolucaoPorData(){
         String query = "select * from pedidos where dataDevolucao = ?";
     }
+
     public void inserirPedidoDB(Pedido pedido){
-        pedido.status =
+        Status statusPedido = pedido.getStatus();
+        statusPedido = "AguardandoEntrega";
+        String query = "insert into pedido values(?,?,?,?)";
+        for(Produto produto : pedido.listaProdutos){
+            String query = "insert into pedidoProdutos(codigo, quantidade, pedidoCodigo, produtoCodigo) values (?,?,?,?)";
+        }
+    }
+    public void editarPedidosDB(){
+        String query = "update pedidoProdutos set (codigo, quantidade, pedidoCodigo, produtoCodigo) values (?,?,?,?)";
     }
 
 }
